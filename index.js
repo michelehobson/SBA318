6115d0266b03c8b5f0bf8d9a64c01568dd2f7ba3
michelehobson <michelehobson@hotmail.com>
2023-11-12 10:08:46 -0500

    Error Handling

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 
 // DEFINE ROUTE CONSTANTS
 const animals = require('./routes/animalsRoute');
+// const {error} = require('console');
 // const architecture = require('./routes/architectureRoute');
 // const landscapes = require('./routes/landscapesRoute');
 // const people = require('./routes/peopleRoute');
@@ -32,22 +33,24 @@ app.use(bodyParser.json({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
-// RENDER THE VIEW - I.E. SHOW THE PAGE
+// RENDER THE HOME PAGE
 app.get('/', (req, res) => {
     res.render('home')
 });
 
-// CATCH 404
+// ERROR HANDLING
+const error = require('./public/middleware/errors')
+
 app.use((req, res, next) => {
-    const err = new Error('Not Found')
-    err.status = 404;
-    next(err);
+    next(error(404, 'Page Not Found'))
 })
 
 app.use((err, req, res, next) => {
-
+    res.status(err.status || 500);
+    res.json({ error: err.message})
 });
 
+// LISTENER
 app.listen(1962, () => {
     console.log(`Listening on the year that God changed the world!`)
 })
--- routes/animalsRoute.js
+++ routes/animalsRoute.js
@@ -2,27 +2,47 @@ const express = require('express');
 const router = express.Router();
 
 const animals = require('../public/data/animals')
+const error = require('../public/middleware/errors')
 const h1 = 'Animals'
-// RETURN FIRST IMAGE
-router.get('/', (req, res) => {
-    // console.log("here")
-    const image = [animals[0].image]
-    // res.render('animals', {image});
-    res.render('template', {image, h1});
-})
 
-// GET SPECIFIC IMAGE
+// APPLICATION LEVEL MIDDLEWARE
+router.get('/', (req, res) => {
+    const image = [animals[0].image]
+    const artist = [animals[0].name]
+    console.log('Request Type:', req.method)
+    console.log('Request URL:', req.originalUrl)
+res.render('template', {image, artist, h1});
+});
+
+router.post('/', (req, res, next) => {
+    if(req.body.image && req.body.name) {
+        if(animals.find((a) => a.image == req.body.image)) {
+            next(error(409), 'Image already exists');
+        }
+        const image = {
+            id: animals[animals.length - 1].id + 1,
+            image: req.body.image,
+            name: req.body.name,
+        }
+
+        animals.push(image);
+        res.json(animals[animals.length - 1]);
+    } else next(error(400, 'The image and photographers name are required'));
+});    
+
 router.get('/:id', (req, res, next) => {
-        const animal = animals.find((a) => a.id == req.params.id);
-        console.log(animal.id)
-        if(animal) {
-            const image = [animals[animal.id].image]
-            const artist = [animals[animal.id].name]
-            // res.render('animals', {image, artist});
-            res.render('template', {image, artist, h1});
-        } else {
-            next()
-        };
-    })
+    const animal = animals.find((a) => a.id == req.params.id);
+    console.log(animal.id)
+    if(animal) {
+        const image = [animals[animal.id].image]
+        const artist = [animals[animal.id].name]
+        res.render('template', {image, artist, h1});
+
+    } else {
+        next()
+    };
+});
+
+
 
 module.exports = router;
